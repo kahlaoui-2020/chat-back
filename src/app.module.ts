9280b9a95e3294a7dd09e9f19991dd88611951b3
kahlaoui-2020 <kahlaoug@gmail.com>
2022-02-11 22:52:52 +0100

    config connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { DiscussionEntity } from './entities/discussion.entity';
+import { MessageEntity } from './entities/message.entity';
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    "type": "mysql",
+    "host": process.env.MYSQL_URL,
+    "port": parseInt(process.env.MYSQL_PORT),
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DB,
+    "entities": [UserEntity, DiscussionEntity, MessageEntity],
+    "synchronize": true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
